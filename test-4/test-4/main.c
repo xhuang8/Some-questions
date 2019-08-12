d285b929fe9c3d674185d2f3f3c332ecb5e21cb8
xhuang8 <xhuang8@mail.sfsu.edu>
2019-08-11 23:44:27 -0700

    added c

--- test-4/test-4/main.c
+++ test-4/test-4/main.c
@@ -7,9 +7,33 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+int* getOneBits(int n, int* result_count){
+    int base = 0;
+    while(n>0){
+        result_count[base++] = n%2;
+        n = n/2;
+    }
+    for(int i = base - 1; i >= 0; i--)
+    {
+        printf("%d", result_count[i]);
+    }
+    printf("\n");
+    int temp = base - 1;
+    for(int i = 0; i < base; i++){
+        if(result_count[i] == 1){
+            printf("%d\n", +(i+1));
+        }
+        temp --;
+    }
+    return 0;
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    int array[40];
+    getOneBits(161, array);
+    //printf("Hello, World!\n");
     return 0;
 }
