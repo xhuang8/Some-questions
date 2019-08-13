a03c15753a8062d1fb1f4a82dcffe02a37cdfe54
xhuang8 <xhuang8@mail.sfsu.edu>
2019-08-12 22:52:28 -0700

    updated

--- test-4/test-4/main.c
+++ test-4/test-4/main.c
@@ -8,26 +8,44 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#import <objc/runtime.h>
 
 int* getOneBits(int n, int* result_count){
     int base = 0;
-    while(n>0){
+    int count = 0;
+    int position = 0;
+    int totalOneBits = 0;
+    
+    /*while(n>0){
         result_count[base++] = n%2;
         n = n/2;
+    }*/
+    
+    for(int i = 0; n > 0; i++){
+        base = base * 10;
+        base = base + n%2;
+        totalOneBits += n%2;
+        n = n/2;
+        count ++;
     }
-    for(int i = base - 1; i >= 0; i--)
+    
+   /* for(int i = 1; i <= count; i++)
     {
         printf("%d", result_count[i]);
     }
     printf("\n");
-    int temp = base - 1;
-    for(int i = 0; i < base; i++){
-        if(result_count[i] == 1){
-            printf("%d\n", +(i+1));
+    int temp = base - 1;*/
+    for(int i = 1; i <= count; i++){
+        if(base%10 == 1){
+            //printf("%d\n", +(i+1));
+            position = position * 10;
+            position = position + i;
         }
-        temp --;
+        base /= 10;
     }
-    return 0;
+    result_count = totalOneBits + position;
+    return result_count;
 }
 
 int main(int argc, const char * argv[]) {
